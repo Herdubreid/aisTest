fb7b8b70145f40bf4fd02d85a557b230c8afd3b6
Finnur Bragason <fbragason@outlook.com>
2019-07-19 16:26:47 +0800

    .

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Celin.AIS;
 using Newtonsoft.Json.Linq;
 
@@ -7,18 +8,19 @@ namespace aisTest
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
             // Initalise E1 Server
-            var e1 = new Server("http://e1.celin.io:9300/jderest/");
+            var e1 = new Celin.AIS.Server("http(s)://server:port/jderest/v2/");
             // Set the authentication parameters
             e1.AuthRequest.deviceName = "aisTest";
             e1.AuthRequest.username = "demo";
-            e1.AuthRequest.password = "testing";
+            e1.AuthRequest.password = "demo";
 
-            // Authenticate
-            if (e1.Authenticate())
+            try
             {
+                // Authenticate
+                await e1.AuthenticateAsync();
                 Console.WriteLine("{0} Logged on {1}!", e1.AuthResponse.userInfo.alphaName, e1.AuthResponse.environment);
 
                 // Create an AB Form Request
@@ -30,36 +32,34 @@ namespace aisTest
                     maxPageSize = "10",
                     formActions = new List<Celin.AIS.Action>()
                 };
-                // Set the Search Type to "C"
-                ab.formActions.Add(new FormAction() { controlID = "54", command = "SetControlValue", value = "C" });
-                // Press the Find Button
-                ab.formActions.Add(new FormAction() { controlID = "15", command = "DoAction" });
+                // Create Form Actions
+                ab.formActions = new[]
+                {
+                    // Set the Search Type to "C"
+                    new FormAction() { controlID = "54", command = "SetControlValue", value = "C" },
+                   // Press the Find Button
+                    new FormAction() { controlID = "15", command = "DoAction" }
+                };
 
                 // Submit the Form Request with a Generic Response Object
-                var genRsp = e1.Request<JObject>(ab);
-                if (genRsp.Item1)
-                {
-                    // Request successful, dumpt the output to the Console
-                    Console.WriteLine(genRsp.Item2);
-                }
+                var genRsp =  await e1.RequestAsync<JObject>(ab);
+                // Request successful, dumpt the output to the Console
+                Console.WriteLine(genRsp);
 
                 // Limit the response to Grid Columns Number and Name
                 ab.returnControlIDs = "1[19,20]";
 
                 // Submit the form Request with our AB class definition
-                var abRsp = e1.Request<AddressBookForm>(ab);
-                if (abRsp.Item1)
+                var abRsp = await e1.RequestAsync<AddressBookForm>(ab);
+                // Print the Grid Items to the Console
+                foreach (var r in abRsp.fs_P01012_W01012B.data.gridData.rowset)
                 {
-                    // Print the Grid Items to the Console
-                    foreach (var r in abRsp.Item2.fs_P01012_W01012B.data.gridData.rowset)
-                    {
-                        Console.WriteLine("{0, 12} {1}", r.mnAddressNumber_19.value, r.sAlphaName_20.value);
-                    }
+                    Console.WriteLine("{0, 12} {1}", r.mnAddressNumber_19.value, r.sAlphaName_20.value);
                 }
             }
-            else
+            catch (Exception e)
             {
-                Console.WriteLine("Authentication failed!");
+                Console.WriteLine("Failed!\n{0}", e.Message);
             }
         }
     }
